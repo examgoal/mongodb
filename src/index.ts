f4b52c06b21f312fb9d03f4d5422b97707e8b055
examgoal <developer.examgoal@gmail.com>
2020-07-05 01:45:48 +0530

    Removed default options

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,65 @@
-import {ClientSession, Db, MongoClient} from 'mongodb'
+import {Db, MongoClient} from 'mongodb'
 import {ConfigOptions} from "./lib/options";
 
 const instances: { [key: string]: MongodbClient } = {};
 
-class MongodbClient{
+class MongodbClient {
 
     private readonly configuration: ConfigOptions;
 
     private readonly mongoClient: MongoClient;
 
-    constructor(config: ConfigOptions){
+    constructor(config: ConfigOptions) {
 
         this.configuration = config;
 
-        this.mongoClient = new MongoClient(config.uri, {...(config.mongodbOptions || {}), numberOfRetries: Number.MAX_VALUE, useNewUrlParser: true, useUnifiedTopology: true});
+        this.mongoClient = new MongoClient(config.uri, config.mongodbOptions || {});
 
     }
 
-    createSession(): ClientSession {
-        if(!this.isConnected){
-
-            throw new Error(`${this.name} Can't create session if client is not connected`);
-
-        }
-        return this.mongoClient.startSession();
-    }
-
-    getDb(name?: string): Promise<Db>{
-       return new Promise((resolve, reject) => {
-             if(this.isConnected){
-                 return resolve(this.mongoClient.db(name || this.configuration.db));
-             }
-             this.mongoClient.connect()
-                 .then(res=>{
-                     if(this.configuration.logEnabled){
-                         console.log(`${this.name} Mongodb Connection Established`);
-                     }
-                     return resolve(res.db(name || this.configuration.db));
-                 })
-                 .catch(reject);
-       });
+    // If you are not using unifiedTopology use this line to connect
+    getDb(name?: string): Promise<Db> {
+        return new Promise((resolve, reject) => {
+            if (this.isConnected) {
+                return resolve(this.mongoClient.db(name || this.configuration.db));
+            }
+            this.mongoClient.connect()
+                .then(res => {
+                    if (this.configuration.logEnabled) {
+                        console.log(`${this.name} Mongodb Connection Established`);
+                    }
+                    return resolve(res.db(name || this.configuration.db));
+                })
+                .catch(reject);
+        });
     }
 
     get db(): Db {
         return this.mongoClient.db(this.configuration.db);
     }
 
-    get isConnected(): boolean{
+    get isConnected(): boolean {
         return this.mongoClient.isConnected();
     }
 
-    get client() : MongoClient{
+    get client(): MongoClient {
         return this.mongoClient;
     }
 
-    get name(){
+    get name() {
         return this.configuration.name || '[DEFAULT]';
     }
 
-    static initializeApp(config: ConfigOptions){
+    static initializeApp(config: ConfigOptions) {
 
         instances[config.name || '[DEFAULT]'] = new MongodbClient(config);
 
     }
 
-    static getInstance(name?: string) : MongodbClient {
-        if(!instances.hasOwnProperty(name || '[DEFAULT]')){
+    static getInstance(name?: string): MongodbClient {
+        if (!instances.hasOwnProperty(name || '[DEFAULT]')) {
 
-            throw new Error((name || '[DEFAULT]')+" MongoDB App is not found");
+            throw new Error((name || '[DEFAULT]') + " MongoDB App is not found");
 
         }
         return instances[name || '[DEFAULT]'];
